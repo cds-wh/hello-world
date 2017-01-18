21898f3fa6d960e95a12001afbe0d89beed6555a
cds-wh <cds-wh@126.com>
2017-01-18 14:45:17 +0800

    teest
    test

---
+++ test.php
@@ -0,0 +1,53 @@
+<?php
+	$a = 'cds';
+	var_dump($a);
+	var_dump(empty($a));
+	unset($a);
+	var_dump($a);
+	var_dump(empty($a));
+	$starttime = microtime(true);
+	$endtime = microtime(true);
+	var_dump(intval($endtime-$starttime));
+
+//var_dump(intval(897.09));
+//var_dump(json_decode('{"name":"76563_1479105511.pdf","url":"result\/77\/76563\/76563_1479105511.pdf","realname":"Kong ZhenyingThe Establishment of Kindergarten \\&#039;s Characteristics in the Wisdom Environmentppt.pdf","size":7.5}',true));
+//$aRedis = new Redis(); 
+//var_dump($aRedis);
+//	$retStr = "为验证您的身份，请使用报名时的手机号进行验证。验证成功后，即完成签到。".chr(10).chr(10)."<a href='http://mp.chinaxueqian.com/CourseNew/my/openId/".'kkk'."/time/".time()."/tid/".'6'."/type/project' style='text-decoration:underline; color:#ff7f00'>立即验证</a>";
+//    echo $retStr;
+//    exit();
+//var_dump(file_get_contents('http://www.baidu.com'));exit();
+//echo intval(0.01*100);
+var_dump(unserialize('a:2:{i:556;a:2:{i:0;s:1:"0";i:1;s:4:"1338";}i:557;a:2:{i:0;s:1:"0";i:1;s:4:"2314";}}'));
+//var_dump(unserialize('a:4:{i:1160;a:2:{i:0;s:7:"208.572";i:1;s:4:"1561";}i:1161;a:2:{i:0;s:1:"0";i:1;s:4:"1229";}i:1162;a:2:{i:0;s:1:"0";i:1;s:4:"2322";}i:1163;a:2:{i:0;s:1:"0";i:1;s:4:"2575";}}'));
+//var_dump(unserialize('a:1:{i:486;a:2:{i:0;s:4:"1808";i:1;s:4:"1808";}}'));
+//var_dump(unserialize('a:7:{i:420;a:2:{i:0;s:1:"0";i:1;s:4:"1276";}i:421;a:2:{i:0;s:1:"0";i:1;s:4:"1737";}i:422;a:2:{i:0;s:1:"0";i:1;s:4:"1718";}i:423;a:2:{i:0;s:1:"0";i:1;s:4:"1714";}i:424;a:2:{i:0;s:1:"0";i:1;s:4:"1091";}i:425;a:2:{i:0;s:1:"0";i:1;s:4:"1344";}i:426;a:2:{i:0;s'));
+//var_dump(unserialize('a:4:{i:338;a:2:{i:0;s:4:"1897";i:1;s:4:"1897";}i:339;a:2:{i:0;s:4:"2125";i:1;s:4:"2125";}i:340;a:2:{i:0;s:4:"1338";i:1;s:4:"1338";}i:341;a:2:{i:0;s:4:"1932";i:1;s:4:"1932";}}'));
+//var_dump(strlen('123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'));
+//var_dump(unserialize('a:4:{i:338;a:2:{i:0;s:4:"1897";i:1;s:4:"1897";}i:339;a:2:{i:0;s:4:"2125";i:1;s:4:"2125";}i:340;a:2:{i:0;s:4:"1338";i:1;s:4:"1338";}i:341;a:2:{i:0;s:4:"1932";i:1;s:4:"1932";}}'));
+//echo md5('12345');
+//$aTest = array();
+//$a = 'ddd';
+//var_dump(!$a);
+//var_dump(is_array($aTest));
+//var_dump(unserialize('a:6:{i:620;a:2:{i:0;s:3:"745";i:1;s:3:"745";}i:615;a:2:{i:0;s:4:"2233";i:1;s:4:"2233";}i:619;a:2:{i:0;s:4:"1764";i:1;s:4:"1764";}i:616;a:2:{i:0;s:4:"2253";i:1;s:4:"2253";}i:617;a:2:{i:0;s:4:"1274";i:1;s:4:"1274";}i:618;a:2:{i:0;N;i:1;N;}}'));
+//if ($aTmp = unserialize($v->studyTime)) {
+//				foreach ($aTmp as $v1) {
+//					$iMy += $v1[0];
+//					$iCount += $v1[1];
+//				}
+//
+//				$aCourseTeacher[$v->courseId]['my'] = self::changeTimeType($iMy);
+//				if ($iMy == $iCount) {
+//					$aCourseTeacher[$v->courseId]['pre'] = '100%';
+//				} elseif ($iCount) {
+//					$aCourseTeacher[$v->courseId]['pre'] = sprintf("%.2f", ($iMy / $iCount) * 100) . '%';
+//
+//				} else {
+//					$aCourseTeacher[$v->courseId]['pre'] = '0%';
+//				}
+//			}
+
+
+
+?>
